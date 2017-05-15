87bc42e212344f1ee07ceb1ea3edbd52c89bd9b7
Andrew Jiang <andrew@Andrews-MacBook-Pro.local>
2017-05-14 23:06:49 -0700

    Adds player data

---
+++ src/static/src/data/players.js
@@ -0,0 +1,30 @@
+"use-strict";
+
+const players = [
+  {
+    'id': 'buster-posey',
+    'name': 'Buster Posey',
+    'number': 28,
+    'position': 'catcher',
+    'country': 'US',
+    'birthdate': '3/27/1987',
+    'image': 'http://mlb.mlb.com/mlb/images/players/head_shot/457763.jpg',
+    'background-image': 'http://mlb.mlb.com/images/players/action_shots/457763.jpg',
+    'awards': [
+      {'year': '2010', 'type': 'NL Rookie of the Year', 'league': 'NL', 'team': 'San Francisco Giants'},
+      {'year': '2010', 'type': 'World Series Championship', 'league': 'NL', 'team': 'San Francisco Giants'},
+      {'year': '2012', 'type': 'World Series Championship', 'league': 'NL', 'team': 'San Francisco Giants'},
+      {'year': '2012', 'type': 'NL MVP', 'league': 'NL', 'team': 'San Francisco Giants'},
+      {'year': '2012', 'type': 'NL All-Star', 'league': 'NL', 'team': 'San Francisco Giants'},
+      {'year': '2012', 'type': 'NL Silver Slugger', 'league': 'NL', 'team': 'San Francisco Giants'},
+      {'year': '2013', 'type': 'NL All-Star', 'league': 'NL', 'team': 'San Francisco Giants'},
+      {'year': '2014', 'type': 'World Series Championship', 'league': 'NL', 'team': 'San Francisco Giants'},
+      {'year': '2014', 'type': 'NL Silver Slugger', 'league': 'NL', 'team': 'San Francisco Giants'},
+      {'year': '2015', 'type': 'NL All-Star', 'league': 'NL', 'team': 'San Francisco Giants'},
+      {'year': '2016', 'type': 'NL All-Star', 'league': 'NL', 'team': 'San Francisco Giants'},
+      {'year': '2016', 'type': 'Rawlings NL Gold Glove', 'league': 'NL', 'team': 'San Francisco Giants'}
+    ]
+  }
+];
+
+export default players;
